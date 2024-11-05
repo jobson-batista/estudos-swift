643e28103b137729115463aac456652f46e656f0
Jobson Batista <51756630+jobson-batista@users.noreply.github.com>
2024-11-05 07:56:56 -0300

    Update desafio0.swift

--- desafio0.swift
+++ desafio0.swift
@@ -3,6 +3,7 @@ import Foundation
 let name = "Steve"
 var lastName: String? = "Jobs"
 print("\(name) \(lastName ?? "Wozniak")")
+// Optional Binding
 if let last = lastName {
     print("\(name) \(last)")
 }
